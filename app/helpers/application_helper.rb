4fae1933066029b45cd82ddd6eeda5246dab96c2
Maloney L <maloneyl@users.noreply.github.com>
2018-05-10 20:43:17 +0100

    add helper method to render components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Our helper will also handle a case when we happen to have two partials
+  # in the same folder with a slightly different functionality
+  # (e.g. _message-form.html.erb and _message-form_admin.html.erb).
+  # As a convention, we will use underscores to tell these “alternative”
+  # partials from each other.
+  def component(component_name, locals = {}, &block)
+    name = component_name.split("_").first
+    render("components/#{name}/#{component_name}", locals, &block)
+  end
+
+  alias c component
 end
