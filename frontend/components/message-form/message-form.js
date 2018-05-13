15e1554a578d2e8a6e203e32bd85e9aed41c641a
Maloney L <maloneyl@users.noreply.github.com>
2018-05-13 17:19:48 +0100

    make sending and receiving messages work

--- frontend/components/message-form/message-form.js
+++ frontend/components/message-form/message-form.js
@@ -1,1 +1,29 @@
+import { sendMessage } from "client/chat";
 import "./message-form.css";
+
+function submitMessage(input) {
+  sendMessage(input.value);
+
+  input.value = ""; // eslint-disable-line no-param-reassign
+  input.focus();
+}
+
+const form = document.querySelector(".js-message-form");
+
+if (form) {
+  const input = form.querySelector(".js-message-form--input");
+  const submit = form.querySelector(".js-message-form--submit");
+
+  input.addEventListener("keydown", event => {
+    if (event.metaKey && event.keyCode === 13) {
+      // Cmd/Ctrl+Enter
+      event.preventDefault();
+      submitMessage(input);
+    }
+  });
+
+  submit.addEventListener("click", event => {
+    event.preventDefault();
+    submitMessage(input);
+  });
+}
--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,2 +1,23 @@
+import { setCallback } from "client/chat";
 import "components/message/message";
 import "./messages.css";
+
+function scrollToBottom(element) {
+  element.scrollTop = element.scrollHeight; // eslint-disable-line no-param-reassign
+}
+
+const messages = document.querySelector(".js-messages");
+
+if (messages) {
+  const content = messages.querySelector(".js-messages--content");
+
+  scrollToBottom(content);
+
+  // Telling `chat.js` to call this piece of code whenever a new message is
+  // received over ActionCable
+  setCallback(message => {
+    content.insertAdjacentHTML("beforeend", message);
+
+    scrollToBottom(content);
+  });
+}
